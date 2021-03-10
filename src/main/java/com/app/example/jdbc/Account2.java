d999e9ff1b0f7168395fde8ba4ec933e3d23604f
Jalal Kiswani <kiswanij@gmail.com>
2021-03-10 09:54:23 +0200

    Initial version

---
+++ src/main/java/com/app/example/jdbc/Account2.java
@@ -0,0 +1,32 @@
+package com.app.example.jdbc;
+
+public class Account2 {
+	int accountId;
+	String accountName;
+	double accountBalance;
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+}
